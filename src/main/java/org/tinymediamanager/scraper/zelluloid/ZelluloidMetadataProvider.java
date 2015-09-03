71224d4261d83b6b59e27c6a46271823d2d33799
Manuel Laggner <manuel.laggner@gmail.com>
2015-09-03 12:06:53 +0200

    changes due to API cleanup

--- src/main/java/org/tinymediamanager/scraper/zelluloid/ZelluloidMetadataProvider.java
+++ src/main/java/org/tinymediamanager/scraper/zelluloid/ZelluloidMetadataProvider.java
@@ -43,9 +43,9 @@ import org.tinymediamanager.scraper.MediaSearchOptions;
 import org.tinymediamanager.scraper.MediaSearchResult;
 import org.tinymediamanager.scraper.MediaTrailer;
 import org.tinymediamanager.scraper.MediaType;
-import org.tinymediamanager.scraper.MetadataUtil;
 import org.tinymediamanager.scraper.UnsupportedMediaTypeException;
 import org.tinymediamanager.scraper.http.CachedUrl;
+import org.tinymediamanager.scraper.util.MetadataUtil;
 import org.tinymediamanager.scraper.util.StrgUtils;
 import org.tinymediamanager.scraper.http.Url;
 
@@ -419,8 +419,6 @@ public class ZelluloidMetadataProvider implements IMovieMetadataProvider, IMovie
           sr.setScore(MetadataUtil.calculateScore(searchTerm, sr.getTitle()));
         }
 
-        // populate extra args
-        MetadataUtil.copySearchQueryToSearchResult(options, sr);
         resultList.add(sr);
       }
       catch (Exception e) {
